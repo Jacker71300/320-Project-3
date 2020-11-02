7349389244e0dead7037fde37777d21152ba9fc2
JTB6154 <jtb6154@rit.edu>
2020-11-02 16:09:44 -0500

    created folder layout, created basic character, created projectile for character
    created folders for assets prefabs and scripts and sounds to go into, as well as imported the singleton abstract class. Created the player character and made it controllable

---
+++ Hexenwolf/Assets/Scripts/Objects/BulletScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+	[SerializeField]Rigidbody2D rb;
+	float speed;
+	[SerializeField] LayerMask damageCollisions;
+	[SerializeField] LayerMask destructionCollisions;
+	[SerializeField] float timeDuration = 20f;
+	Vector2 direction;
+
+
+	private void Start()
+	{
+		GetComponents();
+		Destroy(gameObject, timeDuration); //destroy this after the duration
+	}
+
+	public void Initialize(Vector3 dir, float Speed)
+	{
+		speed = Speed;
+		direction = new Vector2(dir.x,dir.y).normalized;
+		transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+	}
+
+	public void FixedUpdate()
+	{
+		//update the position 60 times a second
+		rb.MovePosition(new Vector2(transform.position.x + direction.x * speed * Time.deltaTime, transform.position.y + direction.y * speed * Time.deltaTime));
+	}
+
+	void GetComponents()
+	{
+		if (rb == null)
+		{
+			rb = GetComponent<Rigidbody2D>();
+		}
+	}
+
+}
---
+++ Hexenwolf/Assets/Scripts/Player Controller/PlayerWeaponController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeaponController : MonoBehaviour
+{
+    [SerializeField] GameObject projectile;
+    [SerializeField] float projectileSpeed = 20f;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            BulletScript Temp = Instantiate(projectile).GetComponent<BulletScript>();
+            Temp.transform.position = transform.position;
+            if (Temp != null)
+            {
+                Temp.Initialize(Camera.main.ScreenToWorldPoint(Input.mousePosition) -transform.position,projectileSpeed);
+            }
+            
+        }
+        
+    }
+}
