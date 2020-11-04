1139866760bfcb604b8278f2f01542e7fab82066
JTB6154 <jtb6154@rit.edu>
2020-11-04 12:58:48 -0500

    changes to testing scene, now camera moves with mouse look
    Camera now moves with mouse look, and there are now four blue markers in the testing scene so i have a sense of the physical space

--- Hexenwolf/Assets/Scripts/Objects/CameraScript.cs
+++ Hexenwolf/Assets/Scripts/Objects/CameraScript.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]GameObject character;
+    [SerializeField][Range(0f,1f)] float minMouseDisplacementRadius = .25f;
+    [SerializeField] [Range(1f, 10f)] float displacementScale = 2f;
     void Start()
     {
-        
+        GetComponents();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 displacement = Utils.Instance.ConvertMousePosToScreenQuarters(Input.mousePosition);
+
+        Vector3 target = new Vector3(character.transform.position.x,
+                                     character.transform.position.y,
+                                     transform.position.z);
+
+        if (displacement.magnitude > minMouseDisplacementRadius)
+        {
+            target += displacement.normalized * Mathf.Lerp(0, displacement.magnitude * displacementScale,
+                                                              displacement.magnitude - minMouseDisplacementRadius);
+            
+        }
+
+        transform.position = target;
+    }
+
+    void GetComponents()
+    {
+        if (character == null)
+        {
+            character = GameObject.Find("PlayerChar");
+        }
     }
 }
