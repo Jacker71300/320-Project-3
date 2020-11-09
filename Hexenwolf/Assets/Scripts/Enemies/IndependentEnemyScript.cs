d97fd38fc989c7c17030fd68f737930dfa7d9a5c
JTB6154 <jtb6154@rit.edu>
2020-11-09 15:23:28 -0500

    stub out basics of AI

---
+++ Hexenwolf/Assets/Scripts/Enemies/IndependentEnemyScript.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AIState
+{
+	Patrol,
+	FollowTrail,
+	PathtoAction,
+	Engage,
+	Wait
+}
+
+public class IndependentEnemyScript : MonoBehaviour
+{
+	AIState state;
+	private void Update()
+	{
+		UpdateAiState();
+
+		switch (state)
+		{
+			case AIState.Patrol:
+
+				break;
+		}
+
+	}
+
+	private void UpdateAiState()
+	{
+
+		if (state != AIState.Engage)
+		{
+
+			if (SpotPlayer())//if we are not engaged already and we've spotted the player
+			{
+				state = AIState.Engage;
+				return;
+			}
+			if (DetectAction())
+			{
+				state = AIState.PathtoAction;
+				return;
+			}
+
+			if (SpotTrail())
+			{
+				state = AIState.FollowTrail;
+				return;
+			}
+		}
+		else if(PlayerEscaped())
+		{
+			state = AIState.Patrol;
+		}
+
+	}
+
+	private bool SpotPlayer()
+	{
+		return false;
+	}
+
+	private bool DetectAction()
+	{
+		return false;
+	}
+
+	private bool SpotTrail()
+	{
+		return false;
+	}
+
+	private bool PlayerEscaped()
+	{
+		return false;
+	}
+}
